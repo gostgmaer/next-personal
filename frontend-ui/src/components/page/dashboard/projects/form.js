32ee8069289672b4a6685b3688dd725b0319049f
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-27 21:10:01 +0530

    ok updated

--- frontend-ui/src/components/page/dashboard/projects/form.js
+++ frontend-ui/src/components/page/dashboard/projects/form.js
@@ -88,7 +88,7 @@ const ProjectForm = ({ initialValues }) => {
     return (
         <div className="container p-2 mx-auto sm:p-4">
             <form className=" mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"     onSubmit={formik.handleSubmit}>
-                <h2 className="text-2xl mb-4 font-semibold">Add Project</h2>
+                <h2 className="text-2xl mb-4 font-semibold">{id?"Update":"Add"} Project</h2>
 
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                     {/* Project Name */}
